9adea15767e4297ed7d9c5adfc795573e681609b
Rogge44 <46192966+Rogge44@users.noreply.github.com>
2018-12-27 13:51:10 +0100

    Create Alexa.py

---
+++ Alexa.py
@@ -0,0 +1,6 @@
+echo "# RaspiAlexa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Rogge44/RaspiAlexa.git
+git push -u origin master
